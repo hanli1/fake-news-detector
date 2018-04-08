afea0bc02367aaa640eca949bc706cdccae15b64
Han Li <hl565@cornell.edu>
2018-04-08 01:06:51 -0400

    pickle featurizer

--- baseline.py
+++ baseline.py
@@ -25,36 +25,40 @@ sys.setdefaultencoding("utf-8")
 
 def main():
     saved_model_file = "baseline_bow.pickle"
+    saved_vectorizer = "baseline_vectorizer.pickle"
     size = 15000
 
+    print "reading data"
     dataset = pd.read_csv("data/combined.csv")
     all_data = dataset['text'][6000:6000+size].values.astype('U')
     all_labels = dataset['real'].tolist()[6000:6000+size]
     all_data = np.array(all_data)
     all_labels = np.array(all_labels)
+
+    print "train test split"
     X_train, X_test, Y_train, Y_true = train_test_split(all_data, all_labels, random_state=42)
     
     if os.path.isfile(saved_model_file):
-        print "loading model"
+        print "loading model and vectorizer"
         clf = pickle.load(open(saved_model_file, 'rb'))
+        vectorizer = pickle.load(open(saved_vectorizer, 'rb'))
     else:
+        print "fitting/saving featurizer"
+        vectorizer = CountVectorizer() 
+        X_train = vectorizer.fit_transform(X_train)
+        pickle.dump(vectorizer, open(saved_vectorizer, 'wb'))
+
         clf = linear_model.LogisticRegression()
         print "fitting/saving model"
         clf.fit(X_train, Y_train)
         pickle.dump(clf, open(saved_model_file, 'wb'))
-
-    print "featurizing"
-    vectorizer = CountVectorizer() 
-    X_train = vectorizer.fit_transform(X_train)
+    
     X_test = vectorizer.transform(X_test)
-
     print "predicting"
     Y_pred = clf.predict(X_test)
-
     results = precision_recall_fscore_support(Y_true, Y_pred, average='macro')
     print results
-
-    print clf.predict(vectorizer.transform(["the earth is flat"]))
+    print clf.predict(vectorizer.transform(["barack obama is the president"]))
 
 if __name__ == '__main__':
     main()
