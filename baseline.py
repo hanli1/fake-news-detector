a04a0e75bb0bfa1d5dbab8fb3c9679132a160f8e
Han Li <hl565@cornell.edu>
2018-04-08 00:53:28 -0400

    fix bugs

--- baseline.py
+++ baseline.py
@@ -25,44 +25,28 @@ sys.setdefaultencoding("utf-8")
 
 def main():
     saved_model_file = "baseline_bow.pickle"
+    size = 15000
 
+    dataset = pd.read_csv("data/combined.csv")
+    all_data = dataset['text'][6000:6000+size].values.astype('U')
+    all_labels = dataset['real'].tolist()[6000:6000+size]
+    all_data = np.array(all_data)
+    all_labels = np.array(all_labels)
+    X_train, X_test, Y_train, Y_true = train_test_split(all_data, all_labels, random_state=42)
+    
     if os.path.isfile(saved_model_file):
+        print "loading model"
         clf = pickle.load(open(saved_model_file, 'rb'))
     else:
         clf = linear_model.LogisticRegression()
-        print "reading data"
-        dataset = pd.read_csv("data/combined.csv")
-        vectorizer = CountVectorizer()
-
-        print "featurizing"
-        size = 15000
-        all_data = dataset['text'][6000:6000+size].values.astype('U')
-        all_labels = dataset['real'].tolist()[6000:6000+size]
-        all_data = np.array(all_data)
-        all_labels = np.array(all_labels)
-
-        X_train, X_test, Y_train, Y_true = train_test_split(all_data, all_labels, random_state=42)
-
-        X_train = vectorizer.fit_transform(X_train)
-        X_test = vectorizer.transform(X_test)
-
-        # print dataset['text'][:size].values.astype('U')
-        # X = []
-        # Y = []
-        # for index, row in dataset.iterrows():
-        #     source = row['publication']
-        #     title = row['title']
-        #     text = row['content']
-        #     real = row['real']
-
-        #     x, y = featurize(source, title, text, real)
-        #     X.append(x)
-        #     Y.append(y)
-        
         print "fitting/saving model"
         clf.fit(X_train, Y_train)
-        pickle.dump(clf, open("baseline_bow.pickle", 'rb'))
+        pickle.dump(clf, open(saved_model_file, 'wb'))
 
+    print "featurizing"
+    vectorizer = CountVectorizer() 
+    X_train = vectorizer.fit_transform(X_train)
+    X_test = vectorizer.transform(X_test)
 
     print "predicting"
     Y_pred = clf.predict(X_test)
@@ -70,7 +54,7 @@ def main():
     results = precision_recall_fscore_support(Y_true, Y_pred, average='macro')
     print results
 
-    print clf.predict(vectorizer.transform(["Hillary is crooked and lies all the time."]))
+    print clf.predict(vectorizer.transform(["the earth is flat"]))
 
 if __name__ == '__main__':
     main()
